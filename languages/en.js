d1aff5bc2e42d26e9940144dbd6ca15db6c36428
Nguyễn Văn Kiên <138760549+KIEN20030806@users.noreply.github.com>
2025-02-25 16:35:10 +0700

    Update

---
+++ languages/en.js
@@ -0,0 +1,8 @@
+Blockly.Msg.BLOCK_SERVO8CHS_WRITE_MESSAGE0 = "%3 servo motor %2%5 rotate %1 speed %4%6"
+Blockly.Msg.BLOCK_SERVO8CHS_WRITE_TOOLTIP = "Rotate servo motor to given position"
+
+Blockly.Msg.BLOCK_SERVO8CHS360_WRITE_MESSAGE0 = "%3 servo 360° %1 speed %2"
+Blockly.Msg.BLOCK_SERVO8CHS360_WRITE_TOOLTIP = "Run servo 360° with given speed"
+
+Blockly.Msg.BLOCK_SERVO8CHS_WRITE_MICRO_MESSAGE0 = "%3servo %2 rotate more %1%4"
+Blockly.Msg.BLOCK_SERVO8CHS_WRITE_MICRO_TOOLTIP = "Servo rotate another more angle"
